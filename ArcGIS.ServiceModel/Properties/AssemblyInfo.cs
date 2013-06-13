ee049e8e6d5957ff2c6050e17d2b2f8314da8869
davetimmins <dwtimmins@outlook.com>
2013-06-14 11:18:19 +1200

    Updated version

--- ArcGIS.ServiceModel/Properties/AssemblyInfo.cs
+++ ArcGIS.ServiceModel/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ArcGIS.ServiceModel")]
-[assembly: AssemblyDescription("Data types used with the ArcGIS REST API")]
+[assembly: AssemblyDescription("Data types used with the ArcGIS Server REST API")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ArcGIS.ServiceModel")]
-[assembly: AssemblyCopyright("Copyright ©Dave Timmins  2013")]
+[assembly: AssemblyProduct("ArcGIS REST API ServiceModel PCL")]
+[assembly: AssemblyCopyright("Copyright Dave Timmins © 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0-alpha")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0-alpha2")]
