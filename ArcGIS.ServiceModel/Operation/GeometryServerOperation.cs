a5b3bf148fec350ef52802bf8d33e06c52d249c6
davetimmins <dwtimmins@outlook.com>
2013-08-05 12:59:59 +1200

    added geojson and geometry tests

--- ArcGIS.ServiceModel/Operation/GeometryServerOperation.cs
+++ ArcGIS.ServiceModel/Operation/GeometryServerOperation.cs
@@ -33,14 +33,20 @@ namespace ArcGIS.ServiceModel.Operation
     [DataContract]
     public class ProjectGeometry<T> : ArcGISServerOperation where T : IGeometry
     {
-        public ProjectGeometry(ArcGISServerEndpoint endpoint)
+        public ProjectGeometry(ArcGISServerEndpoint endpoint, List<Feature<T>> features, SpatialReference outputSpatialReference)
             : base(endpoint, Operations.Project)
         {
-            Geometries = new GeometryCollection<T>();
+            if (features.Any())
+            {
+                Geometries = new GeometryCollection<T>();
+                Geometries.Geometries = features.Select(f => f.Geometry).ToList();
+                Geometries.Geometries.First().SpatialReference = outputSpatialReference;
+            }
+            OutputSpatialReference = outputSpatialReference;
         }
 
         [DataMember(Name = "geometries")]
-        public GeometryCollection<T> Geometries { get; set; }
+        public GeometryCollection<T> Geometries { get; private set; }
 
         [DataMember(Name = "inSR")]
         public SpatialReference InputSpatialReference { get { return Geometries.Geometries.First().SpatialReference ?? SpatialReference.WGS84; } }
@@ -50,7 +56,7 @@ namespace ArcGIS.ServiceModel.Operation
         /// If not specified, the geometry is returned in the spatial reference of the input.
         /// </summary>
         [DataMember(Name = "outSR")]
-        public SpatialReference OutputSpatialReference { get; set; }
+        public SpatialReference OutputSpatialReference { get; private set; }
     }
 
     [DataContract]
---
+++ ArcGIS.Test/GeometryGatewayTests.cs
@@ -0,0 +1,60 @@
+﻿using ArcGIS.ServiceModel.Common;
+using ArcGIS.ServiceModel.Logic;
+using ArcGIS.ServiceModel.Operation;
+using ArcGIS.ServiceModel.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ArcGIS.Test
+{
+    public class GeometryGatewayTests
+    {
+        [Fact]
+        public async Task CanProject()
+        {
+            var gateway = new GeometryGateway();
+            var result = await gateway.Query<Polyline>(new Query("Hurricanes/MapServer/1".AsEndpoint()));
+
+            var features = result.Features.Where(f => f.Geometry.Paths.Any()).ToList();
+
+            Assert.NotNull(features);
+            Assert.True(result.SpatialReference.Wkid != SpatialReference.WGS84.Wkid);
+
+            if (result.SpatialReference.Wkid != SpatialReference.WGS84.Wkid){
+
+                var projectedFeatures = await gateway.Project<Polyline>(features, result.SpatialReference);
+                
+                Assert.NotNull(projectedFeatures);
+                Assert.Equal(features.Count, projectedFeatures.Count);
+            }
+        }
+    }
+
+    public class GeometryGateway : PortalGateway
+    {
+        public GeometryGateway()
+            : base(@"http://sampleserver6.arcgisonline.com/arcgis")
+        {
+            Serializer = new ServiceStackSerializer();
+        }
+
+        public async Task<QueryResponse<T>> Query<T>(Query queryOptions) where T : IGeometry
+        {
+            return await Get<QueryResponse<T>, Query>(queryOptions);
+        }
+
+        public async Task<List<Feature<T>>> Project<T>(List<Feature<T>> features, SpatialReference outputSpatialReference) where T : IGeometry
+        {
+            var op = new ProjectGeometry<T>("/Utilities/Geometry/GeometryServer".AsEndpoint(), features, outputSpatialReference);           
+            var projected = await Post<GeometryOperationResponse<T>, ProjectGeometry<T>>(op);
+            for (int i = 0; i < projected.Geometries.Count; i++)
+                features[i].Geometry = projected.Geometries[i];
+
+            return features;
+        }
+    }
+}
