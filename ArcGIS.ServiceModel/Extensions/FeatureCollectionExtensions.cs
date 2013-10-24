4ddfc5c673fb7a1c7af6c44c523f61623e58d9cb
davetimmins <dwtimmins@outlook.com>
2013-10-24 13:47:57 +1300

    Fix for UpdateGeometries

--- ArcGIS.ServiceModel/Extensions/FeatureCollectionExtensions.cs
+++ ArcGIS.ServiceModel/Extensions/FeatureCollectionExtensions.cs
@@ -88,13 +88,17 @@ namespace ArcGIS.ServiceModel
         public static List<Feature<T>> UpdateGeometries<T>(this List<Feature<T>> features, List<T> geometries) where T : IGeometry
         {
             var result = new List<Feature<T>>();
+
             for (int i = 0; i < features.Count; i++)
             {
-                var attr = i < features.Count  ? features[i].Attributes : null;
+                var attr = i < features.Count ? features[i].Attributes : null;
                 var feature = new Feature<T> { Attributes = attr };
                 if (i < geometries.Count) feature.Geometry = geometries[i];
                 result.Insert(i, feature);
             }
+            if (geometries.Count > features.Count)
+                result.InsertRange(features.Count, geometries.Skip(features.Count).Select(g => new Feature<T> { Geometry = g }));
+
             return result;
         }
     }
