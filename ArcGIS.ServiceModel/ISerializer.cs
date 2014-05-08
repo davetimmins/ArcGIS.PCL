b3ccc2f8880429cbf89e8b7791a38033fd863f91
Dave Timmins <dwtimmins@outlook.com>
2014-05-09 07:57:55 +1200

    Missing files added

---
+++ ArcGIS.ServiceModel/ISerializer.cs
@@ -0,0 +1,30 @@
+﻿using ArcGIS.ServiceModel.Operation;
+using System;
+using System.Collections.Generic;
+
+namespace ArcGIS.ServiceModel
+{
+    /// <summary>
+    /// Used for (de)serializtion of requests and responses. 
+    /// </summary>
+    /// <remarks>Split out as interface to allow injection. Also moves implementation out of this
+    /// library so it can use whatever framework the developer wants.</remarks>
+    public interface ISerializer
+    {
+        /// <summary>
+        /// Convert an object into a dictionary
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="objectToConvert"></param>
+        /// <returns></returns>
+        Dictionary<String, String> AsDictionary<T>(T objectToConvert) where T : CommonParameters;
+
+        /// <summary>
+        /// Deserialize string as a <see cref="IPortalResponse"/>
+        /// </summary>
+        /// <typeparam name="T">The type of the result from the call</typeparam>
+        /// <param name="dataToConvert">Json string to deserialize</param>
+        /// <returns></returns>
+        T AsPortalResponse<T>(String dataToConvert) where T : IPortalResponse;
+    }
+}
