276e07b2283690ebcaee86091012c7cbc2b0efd7
Dave Timmins <dwtimmins@outlook.com>
2014-05-19 11:54:13 +1200

    set version 3.2.0

--- ArcGIS.ServiceModel.Android/Properties/AssemblyInfo.cs
+++ ArcGIS.ServiceModel.Android/Properties/AssemblyInfo.cs
@@ -26,9 +26,9 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.1.0")]
-[assembly: AssemblyFileVersion("3.1.0")]
-[assembly: AssemblyInformationalVersion("3.1.0")]
+[assembly: AssemblyVersion("3.2.0")]
+[assembly: AssemblyFileVersion("3.2.0")]
+[assembly: AssemblyInformationalVersion("3.2.0")]
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
