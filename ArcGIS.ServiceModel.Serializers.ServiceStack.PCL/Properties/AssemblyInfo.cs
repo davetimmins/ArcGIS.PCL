f5c9afd4f090c97141afd67e6d0fa9b23381ed07
Dave Timmins <dwtimmins@outlook.com>
2014-09-26 08:31:36 +1200

    update asm info

--- ArcGIS.ServiceModel.Serializers.ServiceStack.PCL/Properties/AssemblyInfo.cs
+++ ArcGIS.ServiceModel.Serializers.ServiceStack.PCL/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ArcGIS.ServiceModel.Serializers.ServiceStack")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ArcGIS.ServiceModel.Serializers.ServiceStack.PCL")]
+[assembly: AssemblyDescription("ISerializer for ArcGIS.PCL using ServiceStack.Text.PCL.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ArcGIS.ServiceModel.Serializers.ServiceStack")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("ArcGIS REST API ServiceModel PCL ISerializer")]
+[assembly: AssemblyCopyright("Copyright Dave Timmins (c) 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -25,6 +25,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
--- ArcGIS.ServiceModel.Serializers.ServiceStack/Properties/AssemblyInfo.cs
+++ ArcGIS.ServiceModel.Serializers.ServiceStack/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,13 +7,14 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ArcGIS.ServiceModel.Serializers.ServiceStack")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("ISerializer for ArcGIS.PCL using ServiceStack.Text.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ArcGIS.ServiceModel.Serializers.ServiceStack")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("ArcGIS REST API ServiceModel PCL ISerializer")]
+[assembly: AssemblyCopyright("Copyright Dave Timmins (c) 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -31,6 +33,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
