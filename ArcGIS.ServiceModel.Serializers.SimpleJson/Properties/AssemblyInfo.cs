e5990606c885486bc719bb0fa7a92e2ef65ec762
Dave Timmins <dwtimmins@outlook.com>
2014-09-26 09:20:04 +1200

    added simplejson serializer

---
+++ ArcGIS.ServiceModel.Serializers.SimpleJson/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ArcGIS.ServiceModel.Serializers.SimpleJson")]
+[assembly: AssemblyDescription("ISerializer for ArcGIS.PCL using SimpleJson.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ArcGIS REST API ServiceModel PCL ISerializer")]
+[assembly: AssemblyCopyright("Copyright Dave Timmins (c) 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
+[assembly: AssemblyInformationalVersion("0.1.0")]
---
+++ ArcGIS.ServiceModel.Serializers.SimpleJson/SimpleJsonSerializer.cs
@@ -0,0 +1,32 @@
+﻿using ArcGIS.ServiceModel.Operation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ArcGIS.ServiceModel.Serializers
+{
+    public class SimpleJsonSerializer : ISerializer
+    {
+        static ISerializer _serializer = null;
+
+        public static void Init()
+        {
+            _serializer = new SimpleJsonSerializer();
+            SerializerFactory.Get = (() => _serializer ?? new SimpleJsonSerializer());
+        }
+
+        public Dictionary<String, String> AsDictionary<T>(T objectToConvert) where T : CommonParameters
+        {
+            var stringValue = SimpleJson.SerializeObject(objectToConvert, SimpleJson.DataContractJsonSerializerStrategy);
+
+            var result = SimpleJson.DeserializeObject<Dictionary<String, object>>(stringValue, SimpleJson.DataContractJsonSerializerStrategy);
+
+            return result.Where(i => i.Value != null).ToDictionary(k => k.Key, k => k.Value.ToString().Replace("\"", ""));
+        }
+
+        public T AsPortalResponse<T>(String dataToConvert) where T : IPortalResponse
+        {
+            return SimpleJson.DeserializeObject<T>(dataToConvert, SimpleJson.DataContractJsonSerializerStrategy);
+        }
+    }
+}
